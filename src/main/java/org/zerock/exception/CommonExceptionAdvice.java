fb3a91dd0390fe0a0e4405313ab0f7e12af6df46
sebaek <sebaek@gmail.com>
2020-08-27 12:29:04 +0900

    exception

--- src/main/java/org/zerock/exception/CommonExceptionAdvice.java
+++ src/main/java/org/zerock/exception/CommonExceptionAdvice.java
@@ -1,9 +1,12 @@
 package org.zerock.exception;
 
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,7 +21,26 @@ public class CommonExceptionAdvice {
 		log.error(model);
 		return "error_page";
 	}
+	
+	// 159 page
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException e) {
+		return "custom404";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
